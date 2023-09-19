afe6ab836076b396ec66f08c40c90139b633b327
Muhammad ismaeel <muhammad.ismaeel5162@gmail.com>
2023-09-19 17:09:20 +0500

    rebased

--- src/app/dashboardLayout/templates/takeAnalysis/page.js
+++ src/app/dashboardLayout/templates/takeAnalysis/page.js
@@ -1,8 +1,27 @@
+"use client"
 import React from 'react'
-
+import { useReactMediaRecorder } from "react-media-recorder";
 function page() {
+    const { status, startRecording, stopRecording, mediaBlobUrl } =
+    useReactMediaRecorder({ video: { facingMode: 'environment' } });
+  
   return (
-    <div>page</div>
+    <div>
+        <p>{status}</p>
+     <button
+  onClick={startRecording}
+  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+>
+  Start Recording
+</button>
+<button
+  onClick={stopRecording}
+  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+>
+  Stop Recording
+</button>
+      <video src={mediaBlobUrl} controls autoPlay loop />
+    </div>
   )
 }
 
