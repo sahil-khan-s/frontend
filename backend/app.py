62b9c1b92dd6f8640e6bdcda9a1e910748048df3
Muhammad ismaeel <muhammad.ismaeel5162@gmail.com>
2023-09-24 15:34:53 +0500

    backend logic added for saving recorded video

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, Response, jsonify , send_file , request ,send_from_directory
+from flask_cors import CORS 
+import os
+app = Flask(__name__)
+CORS(app)
+UPLOAD_FOLDER=os.path.join(os.getcwd(),'database','videos')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/download', methods = ['POST'])
+def download():
+    file = request.files['video']
+    path=os.path.join(app.config['UPLOAD_FOLDER'], 'recorded_video'+'.webm')
+    file.save(path)
+    response = {'message': "video downloaded successfully"}
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
