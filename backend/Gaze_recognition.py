c30f291da93a197e09478344fc711fa524f70d68
Your Name <you@example.com>
2023-09-27 13:58:42 +0500

    integration done with frontend and error resolve to pitch video from database

--- backend/Gaze_recognition.py
+++ backend/Gaze_recognition.py
@@ -40,8 +40,8 @@ def gaze_detection(video_path):
         left_pupil = gaze.pupil_left_coords()
         right_pupil = gaze.pupil_right_coords()
 
-        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        # cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
+        # cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
         # cv2.imshow("Demo", frame)
 
