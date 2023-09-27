de5a3f4d8c2b1a66c9644516be96ae9d3926c3f3
Muhammad ismaeel <m.ismail@devsort.net>
2023-09-27 23:07:51 +0500

    circular bars added

--- backend/emotion_recognition.py
+++ backend/emotion_recognition.py
@@ -2,21 +2,21 @@ import cv2
 import json
 from deepface import DeepFace
 
-def detect_emotions(video_path, frame_skip = 5):
+def detect_emotions(video_path, frame_skip=5):
     cap = cv2.VideoCapture(video_path)
 
     total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     frame_count = 0
 
     emotion_scores = {
-                        'neutral': 0, 
-                        'happy': 0,
-                        'surprise': 0, 
-                        'angry': 0, 
-                        'disgust': 0, 
-                        'fear': 0, 
-                        'sad': 0 
-                    }
+        'neutral': 0,
+        'happy': 0,
+        'surprise': 0,
+        'angry': 0,
+        'disgust': 0,
+        'fear': 0,
+        'sad': 0
+    }
 
     while True:
         ret, frame = cap.read()
@@ -25,22 +25,20 @@ def detect_emotions(video_path, frame_skip = 5):
 
         frame_count += 1
         if frame_count % frame_skip == 0:
-            result = DeepFace.analyze(frame, actions=['emotion'], 
-                                    enforce_detection=False,  
-                                    detector_backend='ssd'
-                                    )
+            result = DeepFace.analyze(frame, actions=['emotion'],
+                                      enforce_detection=False,
+                                      detector_backend='ssd'
+                                      )
             emotion_scores[result[0]["dominant_emotion"]] += 1
-            # print(result[0]["dominant_emotion"])
         if not ret:
             break
     cap.release()
 
-    emotion_results = {}
+    emotion_results = []
 
     for emotion, count in emotion_scores.items():
         score = (count / 5) * 10
         formatted_score = "{:.2f}".format(score)
-        emotion_results[emotion] = formatted_score
+        emotion_results.append({"name": emotion, "score": formatted_score})
 
-    emotion_json = json.dumps(emotion_results)
-    return emotion_json
+    return emotion_results
