6d97290cc367f38f20ce204c91e5cad2085eb0f9
Muhammad ismaeel <m.ismail@devsort.net>
2023-10-02 12:33:55 +0500

    signup changes

--- backend/auth.py
+++ backend/auth.py
@@ -3,6 +3,9 @@ from flask import Blueprint, request, jsonify
 
 auth_bp = Blueprint('auth', __name__)
 
+# Store registered users in memory as a list of dictionaries
+registered_users = []
+
 @auth_bp.route('/signup', methods=['POST'])
 def signup():
     data = request.form
@@ -11,20 +14,49 @@ def signup():
     password = data.get('password')
 
     try:
-       
-        user_registered_successfully = register_user(name, email, password)
-        if user_registered_successfully:
-            response = {'message': 'User registered successfully'}
-            return jsonify(response), 201
-        else:
-            response = {'message': 'User registration failed'}
+        # Check if the user with the same email already exists
+        existing_user = next((user for user in registered_users if user['email'] == email), None)
+
+        if existing_user:
+            response = {'message': 'User with this email already exists'}
             return jsonify(response), 400
+
+        # Create a user dictionary
+        user_data = {
+            'name': name,
+            'email': email,
+            'password': password,
+        }
+
+        # Store the user data in memory
+        registered_users.append(user_data)
+
+        response = {'message': 'User registered successfully'}
+        return jsonify(response), 201
 
     except Exception as e:
         print(str(e))
         response = {'message': 'User registration failed'}
         return jsonify(response), 500
 
-def register_user(name, email, password):
-   
-    pass
+@auth_bp.route('/login', methods=['POST'])
+def login():
+    data = request.form
+    email = data.get('email')
+    password = data.get('password')
+
+    try:
+        # Find a user with the provided email and password
+        matching_user = next((user for user in registered_users if user['email'] == email and user['password'] == password), None)
+
+        if matching_user:
+            response = {'message': 'Login successful'}
+            return jsonify(response), 200
+        else:
+            response = {'message': 'Login failed. Invalid credentials.'}
+            return jsonify(response), 401
+
+    except Exception as e:
+        print(str(e))
+        response = {'message': 'Login failed'}
+        return jsonify(response), 500
