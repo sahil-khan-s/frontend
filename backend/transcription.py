41566f80fb502af1491b07e74b7e1767d16500bf
Muhammad ismaeel <m.ismail@devsort.net>
2023-10-24 17:47:06 +0500

    path updated

--- backend/transcription.py
+++ backend/transcription.py
@@ -1,29 +1,23 @@
+import moviepy.editor as mp
 import os
 import whisper
 import ffmpeg
-import subprocess
-
+​
 def video_transcribe(video_path):
-    # Converting input webm video to mp4 format
+    #converting input webm video to mp4 format
     output_video_file = video_path.split('.')[0] + ".mp4"
-    
-    input_file = ffmpeg.input(video_path, f='webm')
-    ffmpeg.output(input_file, output_video_file, vcodec='libx264', acodec='aac').run()
-    
+    ffmpeg.input(video_path, f='webm') \
+    .output(output_video_file, vcodec='libx264', acodec='aac') \
+    .run()
     print(f'Video converted and saved as {output_video_file}')
-    
-    video_file = os.path.join(os.getcwd(), output_video_file)
+    video_file = os.path.join(os.getcwd(),output_video_file)
     temp_audio_file = output_video_file.split('.')[0] + ".wav"
-    
     # Extract the audio from the video
-    input_file = ffmpeg.input(video_file)
-    ffmpeg.output(input_file.audio, temp_audio_file).run()
-    
+    video_clip = mp.VideoFileClip(video_file)
+    video_clip.audio.write_audiofile(temp_audio_file)
     # Transcribe the audio to text
     model = whisper.load_model("base.en")
-    result = model.transcribe(os.path.join(os.getcwd(), temp_audio_file))
-    
-    # Remove temporary audio file
+    result = model.transcribe(os.path.join(os.getcwd(),output_video_file))
+    #print(result["text"])
     os.remove(temp_audio_file)
-    
     return result["text"]
